afd981f34c26ad56dd039ac7a49b17ac57e6acd5
noahfellman <gazelle@MacBook-Pro.local>
2015-06-05 10:51:10 -0400

    geoform

---
+++ geoform/config/defaults.js
@@ -0,0 +1,69 @@
+define({
+    //Default configuration settings for the applciation. This is where you"ll define things like a bing maps key,
+    //default web map, default app color theme and more. These values can be overwritten by template configuration settings
+    //and url parameters.
+    "appid": "",
+    "webmap": "0c5cb13c4fc54b28bb26a125221ed96f",
+    "form_layer": {
+        "id": ""
+    },
+    "details": {
+        "Title": "",
+        "Logo": "",
+        "Description": ""
+    },
+    "fields": {
+    },
+    "theme": "bootstrap", // see values in themes.js
+    "oauthappid": null,
+    //Enter the url to the proxy if needed by the applcation. See the "Using the proxy page" help topic for details
+    // //developers.arcgis.com/en/javascript/jshelp/ags_proxy.html
+    "proxyurl": "",
+    //Example of a template specific property. If your template had several color schemes
+    //you could define the default here and setup configuration settings to allow users to choose a different
+    //color theme.
+    //Enter the url to your organizations bing maps key if you want to use bing basemaps
+    "bingmapskey": "",
+    //Defaults to arcgis.com. Set this value to your portal or organization host name.
+    "sharinghost": location.protocol + "//" + "www.arcgis.com",
+    "units": null,
+    "useSmallHeader": false,
+    "enableSharing": true,
+    "defaultMapExtent": true,
+    "pushpinColor": "blue",
+    "nextBasemap": "hybrid",
+    "defaultBasemap": "topo",
+    "enableAttachments": true,
+    "attachmentIsRequired": false,
+    "attachmentLabel": "",
+    "attachmentHelpText": "",
+    "showLayer": true,
+    "disableLogo": false,
+    "locate":false,
+    "locationSearchOptions": {
+        "enableMyLocation": true,
+        "enableSearch": true,
+        "enableLatLng": true,
+        "enableUSNG": false,
+        "enableMGRS": false,
+        "enableUTM": false
+    },
+    "attachmentInfo":{
+    },
+    "helperServices": {
+        "geometry": {
+            "url": null
+        },
+        "printTask": {
+            "url": null
+        },
+        "elevationSync": {
+            "url": null
+        },
+        "geocode": [{
+            "url": null
+        }]
+    },
+    "bitlyLogin": "esri",
+    "bitlyKey": "R_65fd9891cd882e2a96b99d4bda1be00e"
+});

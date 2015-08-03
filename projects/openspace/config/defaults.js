8fe420a8aa4ea402a24ed567b7e17fd8fa75d85a
noahfellman <gazelle@MacBook-Pro.local>
2015-08-03 13:06:13 -0400

    update

--- projects/openspace/config/defaults.js
+++ projects/openspace/config/defaults.js
@@ -28,8 +28,8 @@ define({
   "proxyurl": "",
   "theme": "#f7f8f8",
   "color": "#4c4c4c",
-  "title": "Hello2",
-  "drawerSize": null,
+  "title": "Open Space",
+  "drawerSize": 0,
   "bingKey": "", //Enter the url to your organizations bing maps key if you want to use bing basemaps
   //Defaults to arcgis.com. Set this value to your portal or organization host name.
   "sharinghost": location.protocol + "//" + "www.arcgis.com",
